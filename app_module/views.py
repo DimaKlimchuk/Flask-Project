cba7bdeca872abe5554a036e7f52f2608ecc42e0
Dmytro Klymchuk <dima.klimchuk.2016@gmail.com>
2023-12-14 21:47:45 +0200

    Adding another routing

--- app_module/views.py
+++ app_module/views.py
@@ -1,6 +1,11 @@
 from flask import jsonify
 from app_module import app 
 
+@app.route('/')
+def home():
+    return 'Hello, this is the home page!'
+
+
 @app.route('/healthcheck', methods=['GET'])
 def healthcheck():
     status = "ok"
