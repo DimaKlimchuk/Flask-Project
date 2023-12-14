96a3d9eb68cb5526883839b0d8cdf2c1aadfd1d7
Dmytro Klymchuk <dima.klimchuk.2016@gmail.com>
2023-12-14 22:42:28 +0200

    Add endpoints for user

--- app_module/views.py
+++ app_module/views.py
@@ -1,5 +1,10 @@
-from flask import jsonify
+from flask import jsonify, request
 from app_module import app 
+
+users = []
+categories = []
+expenses = []
+
 
 @app.route('/')
 def home():
@@ -10,3 +15,39 @@ def home():
 def healthcheck():
     status = "ok"
     return jsonify({"status": status})
+
+
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    user = {
+        'id': len(users) + 1,
+        'name': data['name']
+    }
+    users.append(user)
+    return jsonify(user), 201
+
+
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = next((user for user in users if user['id'] == user_id), None)
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({'error': 'User not found'}), 404
+
+
+@app.route('/user/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    global users
+    users = [user for user in users if user['id'] != user_id]
+    return jsonify({'message': 'User deleted successfully'})
+
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    return jsonify(users)
+
+
+
