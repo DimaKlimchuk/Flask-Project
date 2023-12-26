cc8b9f038d0b83a31e11c1ed6c2335059d406a75
Dmytro Klymchuk <dima.klimchuk.2016@gmail.com>
2023-12-26 08:33:13 +0200

    Small fix

--- app_module/schemas.py
+++ app_module/schemas.py
@@ -3,20 +3,20 @@ from datetime import datetime
 
 
 class UserSchema(Schema):
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     balance = fields.Float(dump_only=True)
 
 
 class CategorySchema(Schema):
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
 
 
 class RecordSchema(Schema):
-    id = fields.Str(dump_only=True)
-    user_id = fields.Str(required=True)
-    category_id = fields.Str(required=True)
+    id = fields.Int(dump_only=True)
+    user_id = fields.Int(required=True)
+    category_id = fields.Int(required=True)
     timestamp = fields.DateTime(dump_only=True)
     amount = fields.Float(required=True)
 
